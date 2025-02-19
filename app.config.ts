292464ad08af56edd111145a42f7e52fd6c91f60
Nodir Xakimov <nodirsayfullayevich@gmail.com>
2025-02-19 12:26:28 +0500

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
+export default defineAppConfig({
+  docus: {
+    title: 'Regions API',
+    description: 'Detailed Information on Uzbekistan’s Regions, Capitals, Districts and Villages.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'nodirxakimov'
+    },
+    github: {
+      dir: '.starters/default/content',
+      branch: 'main',
+      repo: 'docus',
+      owner: 'nuxt-themes',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: false,
+      fluid: false
+    },
+    header: {
+      logo: false,
+      showLinkIcon: false,
+      exclude: [],
+      fluid: false
+    }
+  }
+})
