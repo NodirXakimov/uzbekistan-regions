fb6e95d62938995248eab845a7720cc2af296f75
Nodir Xakimov <nodirsayfullayevich@gmail.com>
2025-02-21 09:53:20 +0500

    Some fields added to regions

--- server/data/regions.ts
+++ server/data/regions.ts
@@ -1,58 +1,114 @@
 export default [
     {
-        "id": 1,
-        "name": "Qoraqalpog‘iston Respublikasi"
+        id: 1,
+        name: "Qoraqalpog‘iston Respublikasi",
+        capital: "Nukus shahri",
+        area: 166600,
+        population: 2000000,
+        carPlate: "95-99"
     },
     {
-        "id": 2,
-        "name": "Andijon viloyati"
+        id: 2,
+        name: "Andijon viloyati",
+        capital: "Andijon shahri",
+        area: 4200,
+        population: 3150000,
+        carPlate: "60-69"
     },
     {
-        "id": 3,
-        "name": "Buxoro viloyati"
+        id: 3,
+        name: "Buxoro viloyati",
+        capital: "Buxoro shahri",
+        area: 39400,
+        population: 1890000,
+        carPlate: "80-84"
     },
     {
-        "id": 4,
-        "name": "Jizzax viloyati"
+        id: 4,
+        name: "Jizzax viloyati",
+        capital: "Jizzax shahri",
+        area: 21200,
+        population: 1430000,
+        carPlate: "25-29"
     },
     {
-        "id": 5,
-        "name": "Qashqadaryo viloyati"
+        id: 5,
+        name: "Qashqadaryo viloyati",
+        capital: "Qarshi shahri",
+        area: 28600,
+        population: 3300000,
+        carPlate: "70-74"
     },
     {
-        "id": 6,
-        "name": "Navoiy viloyati"
+        id: 6,
+        name: "Navoiy viloyati",
+        capital: "Navoiy shahri",
+        area: 111000,
+        population: 1000000,
+        carPlate: "85-89"
     },
     {
-        "id": 7,
-        "name": "Namangan viloyati"
+        id: 7,
+        name: "Namangan viloyati",
+        capital: "Namangan shahri",
+        area: 7900,
+        population: 2800000,
+        carPlate: "50-59"
     },
     {
-        "id": 8,
-        "name": "Samarqand viloyati"
+        id: 8,
+        name: "Samarqand viloyati",
+        capital: "Samarqand shahri",
+        area: 16400,
+        population: 3900000,
+        carPlate: "30-39"
     },
     {
-        "id": 9,
-        "name": "Surxandaryo viloyati"
+        id: 9,
+        name: "Surxandaryo viloyati",
+        capital: "Termiz shahri",
+        area: 20100,
+        population: 2500000,
+        carPlate: "75-79"
     },
     {
-        "id": 10,
-        "name": "Sirdaryo viloyati"
+        id: 10,
+        name: "Sirdaryo viloyati",
+        capital: "Guliston shahri",
+        area: 4300,
+        population: 870000,
+        carPlate: "20-24"
     },
     {
-        "id": 11,
-        "name": "Toshkent viloyati"
+        id: 11,
+        name: "Toshkent viloyati",
+        capital: "Nurafshon shahri",
+        area: 15300,
+        population: 3100000,
+        carPlate: "10-19"
     },
     {
-        "id": 12,
-        "name": "Farg‘ona viloyati"
+        id: 12,
+        name: "Farg‘ona viloyati",
+        capital: "Farg‘ona shahri",
+        area: 6800,
+        population: 3700000,
+        carPlate: "40-49"
     },
     {
-        "id": 13,
-        "name": "Xorazm viloyati"
+        id: 13,
+        name: "Xorazm viloyati",
+        capital: "Urganch shahri",
+        area: 6300,
+        population: 1800000,
+        carPlate: "90-94"
     },
     {
-        "id": 14,
-        "name": "Toshkent shahri"
+        id: 14,
+        name: "Toshkent shahri",
+        capital: "Toshkent shahri",
+        area: 335,
+        population: 2700000,
+        carPlate: "01-09"
     }
 ]
